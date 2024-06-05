767265864eee75980ac055326b734e1ef9340700
user <saidakrom785@gmail.com>
2024-06-05 17:57:51 +0500

    Initial commit

---
+++ all bots/state.py
@@ -0,0 +1,26 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+class Value(StatesGroup):
+    value = State()
+    kurs = State()
+
+
+class File(StatesGroup):
+    out = State()
+
+
+class Wiki(StatesGroup):
+    loop = State()
+
+class Tel(StatesGroup):
+    loop = State()
+
+
+class Download_yt(StatesGroup):
+    loop = State()
+
+
+class Trans(StatesGroup):
+    loop = State()
+    lang = State()
